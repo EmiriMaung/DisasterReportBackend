a8470803015ba7a1c0630f79a60fc1b50c6ad1e0
Candy13506 <yaminoo13506@gmail.com>
2025-07-17 10:13:52 +0630

    disastertopic

---
+++ DisasterReport.Services/Services/Interfaces/IDisasterTopicService.cs
@@ -0,0 +1,18 @@
+﻿using DisasterReport.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisasterReport.Services.Services.Interfaces
+{
+    public interface IDisasterTopicService
+    {
+        Task<List<DisasterTopicDto>> GetAllAsync();
+        Task<DisasterTopicDto?> GetByIdAsync(int id);
+        Task<DisasterTopicDto> CreateAsync(CreateDisasterTopicDto dto);
+        Task<bool> UpdateAsync(UpdateDisasterTopicDto dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
