eb3c552a935c978654bae96405ca5821c478d3f7
EmiriMaung <poelay1995@gmail.com>
2025-08-25 17:24:43 +0630

    ShowPendingDonation

--- DisasterReport.Services/Models/DonationDto.cs
+++ DisasterReport.Services/Models/DonationDto.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public int DonateRequestId { get; set; }
         public DateTime DonatedAt { get; set; }
+        public string DonatedByUserName { get; set; } = default!;
+        public decimal Amount { get; set; }
     }
 }
